293a923f9665161142b1f2796dabf1a65716beb6
syed imad <syedimad@syeds-MacBook-Pro.local>
2020-07-29 17:38:51 -0500

    Added queries based on date for each specific time slot and added weekend feature with updated times

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timedelta
 from sqlalchemy import text
 
 
@@ -48,11 +48,60 @@ def index():
             return 'There was an issue adding your task'
 
     else:
-        slots1 = Todo.query.filter_by(slot='Slot1').all()
-        slots2 = Todo.query.filter_by(slot='Slot2').all()
-        slots3 = Todo.query.filter_by(slot='Slot3').all()
-        slots4 = Todo.query.filter_by(slot='Slot4').all()
-        slots5 = Todo.query.filter_by(slot='Slot5').all()
+        filter_before = datetime.today() - timedelta(days = 1)
+        filter_after = datetime.today() + timedelta(days = 1)
+        tasks=Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).all()
+        count=Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).count()
+        slots1 =Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot1').all()
+        slots2 = Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot2').all()
+        slots3 = Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot3').all()
+        slots4 = Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot4').all()
+        sum_slot1=0
+        sum_slot2=0
+        sum_slot3=0
+        sum_slot4=0
+        # sum_slot5=0
+        for slot in slots1:
+            sum_slot1= sum_slot1 + slot.no_adults + slot.children
+        for slot in slots2:
+            sum_slot2= sum_slot2 + slot.no_adults + slot.children
+        for slot in slots3:
+            sum_slot3= sum_slot3 + slot.no_adults + slot.children
+        for slot in slots4:
+            sum_slot4= sum_slot4 + slot.no_adults + slot.children
+        # for slot in slots5:
+        #     sum_slot5= sum_slot5 + slot.no_adults + slot.children
+        # tasks = Todo.query.order_by(Todo.time_created).all()
+        return render_template('index.html', tasks=tasks,sum_slot1=sum_slot1,sum_slot2=sum_slot2,sum_slot3=sum_slot3,sum_slot4=sum_slot4,count=count)
+
+
+@app.route('/Weekends', methods=['POST', 'GET'])
+def Weekends():
+    if request.method == 'POST':
+        name = request.form['name']
+        slot = request.form['slot']
+        room_no = request.form['room_no']
+        no_adults = request.form['no_adults']
+        children = request.form['children']
+        new_task = Todo(name=name,slot=slot,room_no=room_no,no_adults=no_adults,children=children)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        filter_before = datetime.today() - timedelta(days = 1)
+        filter_after = datetime.today() + timedelta(days = 1)
+        tasks=Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).all()
+        count=Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).count()
+        slots1 =Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot1').all()
+        slots2 = Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot2').all()
+        slots3 = Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot3').all()
+        slots4 = Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot4').all()
+        slots5 = Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot5').all()
         sum_slot1=0
         sum_slot2=0
         sum_slot3=0
@@ -68,9 +117,44 @@ def index():
             sum_slot4= sum_slot4 + slot.no_adults + slot.children
         for slot in slots5:
             sum_slot5= sum_slot5 + slot.no_adults + slot.children
-        tasks = Todo.query.order_by(Todo.time_created).all()
-        return render_template('index.html', tasks=tasks,sum_slot1=sum_slot1,sum_slot2=sum_slot2,sum_slot3=sum_slot3,sum_slot4=sum_slot4,sum_slot5=sum_slot5)
+        return render_template('Weekends.html', tasks=tasks,sum_slot1=sum_slot1,sum_slot2=sum_slot2,sum_slot3=sum_slot3,sum_slot4=sum_slot4,sum_slot5=sum_slot5)
 
+@app.route('/filter_date_page', methods=['POST', 'GET'])
+def filter_date_page():
+    if request.method == 'POST':
+        # name = request.form['name']
+        # slot = request.form['slot']
+        # room_no = request.form['room_no']
+        # no_adults = request.form['no_adults']
+        # children = request.form['children']
+        # new_task = Todo(name=name,slot=slot,room_no=room_no,no_adults=no_adults,children=children)
+        #
+        # try:
+        #     db.session.add(new_task)
+        #     db.session.commit()
+        #     return redirect('/')
+        # except:
+        #     return 'There was an issue adding your task'
+        # dt_from = datetime(2020, 7, 12, 22, 5, 20)
+        # dt_to = datetime(2020, 7, 24)
+        date = request.form['filter_date']
+        dt_from = datetime.strptime(date, '%Y-%m-%d')
+        # filter_before = datetime.today() - timedelta(days = 1)
+        filter_before = dt_from - timedelta(days = 1)
+        filter_after = dt_from + timedelta(days = 1)
+        # date_from = datetime.strptime(date.strftime('%Y,%m,%d'), '%Y,%m,%d')
+        # dt_from = datetime(date_from)
+        # dt_to = datetime(2020, 7, 24)
+
+        tasks=Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after))
+
+        # tasks = Todo.query.filter_by(Todo.name = 'imad').all()
+        # tasks = Todo.query.filter(Todo.time_created.strftime("%Y-%m-%d") = date).all()
+        # tasks = Todo.query.filter(Todo.time_created == date).all()
+        return render_template('filtered.html', tasks=tasks)
+
+    else:
+        return redirect('/')
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -106,31 +190,41 @@ def update(id):
 def slot1():
     # task = Todo.query.filter(Todo.slot == 'Slot1')
     # tasks = Todo.query.order_by(Todo.time_created).all()
-    tasks = Todo.query.filter_by(slot='Slot1').all()
-    return render_template('slot1.html', tasks=tasks)
+    filter_before = datetime.today() - timedelta(days = 1)
+    filter_after = datetime.today() + timedelta(days = 1)
+    tasks=Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot1').all()
+    return render_template('slot.html', tasks=tasks)
     # task = Todo.query.filter_by(slot='Slot1').all()
     # task = Todo.query.get_or_404(id)
     # return render_template('slot1.html', task=task)
 
 @app.route('/slot2', methods=['GET', 'POST'])
 def slot2():
-    tasks = Todo.query.filter_by(slot='Slot2').all()
-    return render_template('slot1.html', tasks=tasks)
+    filter_before = datetime.today() - timedelta(days = 1)
+    filter_after = datetime.today() + timedelta(days = 1)
+    tasks = Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot2').all()
+    return render_template('slot.html', tasks=tasks)
 
 @app.route('/slot3', methods=['GET', 'POST'])
 def slot3():
-    tasks = Todo.query.filter_by(slot='Slot3').all()
-    return render_template('slot1.html', tasks=tasks)
+    filter_before = datetime.today() - timedelta(days = 1)
+    filter_after = datetime.today() + timedelta(days = 1)
+    tasks = Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot3').all()
+    return render_template('slot.html', tasks=tasks)
 
 @app.route('/slot4', methods=['GET', 'POST'])
 def slot4():
-    tasks = Todo.query.filter_by(slot='Slot4').all()
-    return render_template('slot1.html', tasks=tasks)
+    filter_before = datetime.today() - timedelta(days = 1)
+    filter_after = datetime.today() + timedelta(days = 1)
+    tasks = Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot4').all()
+    return render_template('slot.html', tasks=tasks)
 
 @app.route('/slot5', methods=['GET', 'POST'])
 def slot5():
-    tasks = Todo.query.filter_by(slot='Slot5').all()
-    return render_template('slot1.html', tasks=tasks)
+    filter_before = datetime.today() - timedelta(days = 1)
+    filter_after = datetime.today() + timedelta(days = 1)
+    tasks = Todo.query.filter(db.between(Todo.time_created, filter_before, filter_after)).filter_by(slot='Slot5').all()
+    return render_template('slot.html', tasks=tasks)
 
 @app.context_processor
 def utility_functions():
@@ -140,22 +234,22 @@ def utility_functions():
     return dict(mdebug=print_in_console)
 
 
-def display_slot(condition):
-    # if session.get("email"):
-    #     curl = mysql.connection.cursor()
-    #     curl.execute("SELECT * FROM freelancers WHERE domain LIKE %s", [condition])
-    #     data = curl.fetchall()
-    #     curl.close()
-    #     return render_template(
-    #         "find_artist.html",
-    #         freelancers=data)
-    # else:
-    #     return redirect(url_for('login'))
-    sql = text('select name from TODO')
-    result = db.engine.execute(sql)
-    names = [row[0] for row in result]
-    print(names)
-    return result
+# def display_slot(condition):
+#     # if session.get("email"):
+#     #     curl = mysql.connection.cursor()
+#     #     curl.execute("SELECT * FROM freelancers WHERE domain LIKE %s", [condition])
+#     #     data = curl.fetchall()
+#     #     curl.close()
+#     #     return render_template(
+#     #         "find_artist.html",
+#     #         freelancers=data)
+#     # else:
+#     #     return redirect(url_for('login'))
+#     sql = text('select name from TODO')
+#     result = db.engine.execute(sql)
+#     names = [row[0] for row in result]
+#     print(names)
+#     return result
 
 
 # @app.route('/Slot1',methods=['POST'])
